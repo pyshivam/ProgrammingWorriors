7d6461456de33a67aea601b7d307bbe9c144f2cc
Shivam Gupta <sg129718@gmail.com>
2018-10-08 19:12:30 +0530

    improvements and updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, jsonify, request
+import random
 
 app = Flask(__name__)
 
@@ -6,6 +7,31 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("home.html")
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/forgot')
+def forgot_password():
+    return "tu password bhul gya great man"
+
+
+@app.route('/authorize', methods=['POST'])
+def authorize():
+    data = request.get_json()
+    tname = data['t_name']
+    print(data, "".join(tname.split()))
+    s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    passwd = "".join(random.sample(s, 6))
+    return jsonify({"data": data, "username": "".join(tname.split()).lower(), "password": passwd})
 
 
 if __name__ == '__main__':
